c68c1fe38238c8ad8a512eb8af1f66eb33caec8b
Red Knight <127162163+RedKnight88@users.noreply.github.com>
2023-12-06 17:54:51 -0500

    local storage, city history, rendering on page

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,98 @@
-/**
- * Let's think up general strategy for this code... a psuedocode if you may.
- * 
- * First, we event listen for the "search button" (make it an ID'd button you dingaling)
- * When the button gets pressed, fetch the data. (well actually, the first thing we should do is check if the data is already there in local storage)
- * Remember, the data has to go through two APIs or something (i think), so we'll be 
- *  getting a city name
- *  then have to translate that to a coordinates (that relate to a station?) 
- *  then grab the city's weather data
- * When we get the weather data, 
- *  we slot it into the cards (they probably shouldn't be there to start? maybe display hide them?)
- *  we slot it into local storage (using the city name as the key)
- *  we create a button with an event listener to reload that local storage data
- * 
- * ... that doesn't seem soooo bad right? should also be able to reuse code from last couple assignments, at least for help with storage and interacting with DOM
- */
-
-// https://api.openweathermap.org/geo/1.0/direct?q=[INPUT TEXT HERE]&limit=5&appid=5971b963d4e77a5142b39cd0020e4736
-// ^^ grabs info about location (specifically lat and long) based on text
-
-//https://api.openweathermap.org/data/2.5/forecast?lat=[INPUT LAT]&lon=[INPUT LON]&appid=5971b963d4e77a5142b39cd0020e4736
-// ^^ grabs weather info
-
 var textInput = document.querySelector("#city-search");
 var searchBtn = document.querySelector("#search-btn");
 
 searchBtn.addEventListener('click', APICall);
 
+var localHistory = JSON.parse(localStorage.getItem('reports'));
+if (localHistory != null) {
+    for (x = 0; x < localHistory.length; x++) {
+        addHistory(localHistory[x].city.name);
+    }
+}
+
 function APICall(event) {
     event.preventDefault();
     var cityName = textInput.value;
-    var retrieveCoordsURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=5971b963d4e77a5142b39cd0020e4736";
+    var goFetch = true;
+    var updatedHistory = JSON.parse(localStorage.getItem('reports'));
+    if (updatedHistory != null) {
+        for (y = 0; y < updatedHistory.length; y++) {
+            if (cityName == updatedHistory[y].city.name) {
+                findForecast(cityName);
+                goFetch = false;
+            }
+        }
+    }
 
-    fetch(retrieveCoordsURL)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            // render city name at beginning of top portion
-            var longitude = data[0]["lon"];
-            var latitude = data[0]["lat"];
-            var retrieveWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=5971b963d4e77a5142b39cd0020e4736";
-            fetch(retrieveWeatherURL)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data) {
-                    console.log(data.list[0].main.temp);//list[i].main.temp | list[i].wind.sppeed | list[i].dt (unicode)
-                // render top portion
-                // for loop to render cards
-                });
-        });
+    if (goFetch) {
+        var retrieveCoordsURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=5971b963d4e77a5142b39cd0020e4736";
+
+        fetch(retrieveCoordsURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                var longitude = data[0]["lon"];
+                var latitude = data[0]["lat"];
+                var retrieveWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=5971b963d4e77a5142b39cd0020e4736&units=imperial";
+                fetch(retrieveWeatherURL)
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        loadForecast(data);
+                        storeForecast(data);
+                        addHistory(data.city.name);
+                        console.log(data); // list[i].dt (unicode)
+                    });
+            });
+    }
+}
+
+function loadForecast(report) {
+    document.querySelector("#city-name").textContent = "Current Weather in " + report.city.name; // ADD DATE
+    document.querySelector("#main-temp").textContent = "Temp: " + report.list[0].main.temp + " F";
+    document.querySelector("#main-wind").textContent = "Wind: " + report.list[0].wind.speed + " mph";
+    document.querySelector("#main-humid").textContent = "Humidity: " + report.list[0].main.humidity + " %";
+    document.querySelector("#main-img").setAttribute('src',"https://openweathermap.org/img/wn/" + report.list[0].weather[0].icon + "@2x.png");
+    // render top portion
+                
+    // for loop to render cards
+    var forecastEl = document.querySelector("#forecast");
+    for (p = 0; p < 5; p++) {
+        // ADD DATE
+        forecastEl.children[p+1].children[1].setAttribute('src',"https://openweathermap.org/img/wn/" + report.list[p].weather[0].icon + "@2x.png");
+        forecastEl.children[p+1].children[2].textContent = "Temp: " + report.list[p].main.temp + " F";
+        forecastEl.children[p+1].children[3].textContent = "Wind: " + report.list[p].wind.speed + " mph";
+        forecastEl.children[p+1].children[4].textContent = "Humidity: " + report.list[p].main.humidity + " %";
+    }
+}
+
+function storeForecast(report) {
+    if (localStorage.getItem('reports') != null) {
+        const reportArray = JSON.parse(localStorage.getItem('reports'));
+        reportArray.push(report);
+        localStorage.setItem('reports', JSON.stringify(reportArray));
+    } else {
+        const newArray = [report];
+        localStorage.setItem('reports', JSON.stringify(newArray));
+    }
+}
+
+function addHistory(city) {
+    var newBtn = document.createElement('button');
+    newBtn.textContent = city;
+    newBtn.setAttribute('class', "w-100 my-1 text-light border-2 bg-secondary rounded-pill");
+    document.body.children[1].children[0].children[2].appendChild(newBtn);
+    newBtn.addEventListener('click', function() {findForecast(city)});
+}
+
+function findForecast(reportName) {
+    console.log("find function passed")
+    var storedArray = JSON.parse(localStorage.getItem('reports'));
+    for (n = 0; n < storedArray.length; n++) {
+        if (reportName == storedArray[n].city.name) {
+            loadForecast(storedArray[n]);
+        }
+    }
 }
